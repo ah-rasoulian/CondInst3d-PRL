125a97bca44000c815636203c15d8ef6aeed86b5
Amirhossein Rasoulian <ahrasoulian@neurorx.com>
2026-02-24 14:00:59 +0000

    add metrics to package.

--- condinst3d/evaluator/metrics/__init__.py
+++ condinst3d/evaluator/metrics/__init__.py
@@ -1,2 +1,4 @@
 from .average_precision import AveragePrecision
 from .detection_cfm import DetectionConfusionMatrix
+from .gcir import GlobalConfluentInstanceRecall
+from .froc import DetectionFROC
